a71257c260192e3d1ba6b93132cae69395b3fb3e
kimpenguin <kpeng@nycopportunity.nyc.gov>
2021-03-10 12:25:21 -0500

    updating all svgs with currentColor fill; renaming svgs

---
+++ config/svgs.js
@@ -0,0 +1,36 @@
+/**
+ * Dependencies
+ */
+
+/**
+ * Config
+ *
+ * @type {Object}
+ */
+module.exports = {
+  /**
+   * Prefix to prepend to optimized svgs
+   *
+   * @type {String}
+   */
+  prefix: '',
+
+  /**
+   * Plugin options for SVGO
+   *
+   * @source https://github.com/svg/svgo#what-it-can-do
+   */
+  svgo: {
+    convertPathData: false,
+  },
+
+  /**
+   * Plugin options for svgstore
+   *
+   * @source https://github.com/svgstore/svgstore#options
+   */
+  svgstore: {
+    /** Filename of the sprite. It will not use the prefix defined above. */
+    file: 'svgs.svg'
+  }
+};
