c34335735df7505d6da8a2b2f00a42055f75b3ca
Yonas Fesehatsion <yone920@gmail.com>
2021-05-14 16:36:45 -0400

    recaptcha added to serach.js

--- src/objects/search/Search.js
+++ src/objects/search/Search.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import MissPlete from 'miss-plete-js';
+import $ from 'jquery';
+import Cookies from 'js-cookie';
 
 class Search {
-  constructor() {
+  constructor(el) {
 	}
   /**
    * Initializes the module
@@ -16,6 +18,10 @@ class Search {
     for (let i = this._inputs.length - 1; i >= 0; i--) {
       this._suggestions(this._inputs[i]);
     }
+
+    $(this._el).on('submit', e => {
+      this._initRecaptcha()
+    })
   }
 
   /**
@@ -31,10 +37,52 @@ class Search {
       className: input.dataset.jsSearchDropdownClass
     });
   }
+
+    /**
+   * Asynchronously loads the Google recaptcha script and sets callbacks for
+   * load, success, and expiration.
+   * @private
+   * @return {this} Screener
+   */
+     _initRecaptcha() {
+      const $script = $(document.createElement('script'));
+      $script.attr('src',
+          'https://www.google.com/recaptcha/api.js' +
+          '?onload=screenerCallback&render=explicit').prop({
+        async: true,
+        defer: true
+      });
+
+      window.screenerCallback = () => {
+        window.grecaptcha.render(document.getElementById('screener-recaptcha'), {
+          'sitekey' : '6LekICYUAAAAAOR2uZ0ajyWt9XxDuspHPUAkRzAB',
+          //Below is the local host key
+          // 'sitekey' : '6LcAACYUAAAAAPmtvQvBwK89imM3QfotJFHfSm8C',
+          'callback': 'screenerRecaptcha',
+          'expired-callback': 'screenerRecaptchaReset'
+        });
+        this._recaptchaRequired = true;
+      };
+
+      // window.screenerRecaptcha = () => {
+      //   this._recaptchaVerified = true;
+      //   $(this._el).parents('.c-tip-ms__topics').removeClass('recaptcha-js');
+      // };
+
+      // window.screenerRecaptchaReset = () => {
+      //   this._recaptchaVerified = false;
+      //   $(this._el).parents('.c-tip-ms__topics').addClass('recaptcha-js');
+      // };
+
+      this._recaptchaRequired = true;
+      $('head').append($script);
+      return this;
+    }
 }
 
 Search.selectors = {
-  MAIN: '[data-js*="search"]'
+  MAIN: '[data-js*="search"]',
+  FORM: 'js-search-form'
 };
 
 export default Search;
