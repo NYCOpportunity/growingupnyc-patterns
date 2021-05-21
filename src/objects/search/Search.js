e6664a3bc0e4a5826c448cb3306f0851ccea046f
Yonas Fesehatsion <yone920@gmail.com>
2021-05-21 12:43:56 -0400

    search recaptcha updated, jQuery dependency removed

--- src/objects/search/Search.js
+++ src/objects/search/Search.js
@@ -1,11 +1,21 @@
 'use strict';
 
 // import MissPlete from 'miss-plete-js';
-import $ from 'jquery';
 import Cookies from 'js-cookie';
 
 class Search {
-  constructor() {
+  constructor(settings) {
+
+    this._settings = {
+      selector: (settings.selector) ? settings.selector : Search.MAIN_INPUT,
+      hiddenInputSelector: (settings.hiddenInputSelector) ? settings.hiddenInputSelector : Search.HIDDEN_INPUT,
+      expiration: (settings.expirationDays) ? settings.expirationDays : Search.EXPIRATION_DAYS,
+      cookieName: (settings.cookieName) ? settings.cookieName : Search.COOKIE_NAME,
+      message: (settings.recaptchaMessage) ? settings.recaptchaMessage : Search.RECAPTCHA_MESSAGE,
+      recaptchaContainerId: (settings.recaptchaContainerId) ? settings.recaptchaContainerId : Search.RECAPTCHA_CONTAINER_ID,
+      siteKey: (settings.siteKey) ? settings.siteKey : Search.SITE_KEY,
+      viewCount: (settings.viewCount) ? settings.viewCount : Search.VIEW_COUNT,
+    };
 
     /** @private {boolean} Whether this component has been initialized. */
     this._initialized = false;
@@ -25,24 +35,21 @@ class Search {
    * Initializes the module
    */
   init() {
-    let viewCount = Cookies.get('searchresultVisits') ?
-    parseInt(Cookies.get('searchresultVisits'), 10) : 0;
-    if (viewCount >= 2 && !this._recaptchainit && window.location.search) {
+    let viewCount = Cookies.get(this._settings.cookieName) ?
+    parseInt(Cookies.get(this._settings.cookieName), 10) : 0;
+    if (viewCount >= this._settings.viewCount && !this._recaptchainit && window.location.search) {
       this._initRecaptcha();
       this._recaptchainit = true;
     };
-    window.location.search && Cookies.set('searchresultVisits', ++viewCount, {expires: (2/1440)});
+    window.location.search && Cookies.set(this._settings.cookieName, ++viewCount, {expires: this._settings.expiration});
 
     if (this._recaptchaRequired && !this._recaptchaVerified && window.location.search) {
       this._addNotification();
-      this._inputs = document.querySelectorAll(Search.selectors.MAIN);
-      for (let i = this._inputs.length - 1; i >= 0; i--) {
-        this._inputs[i].name = "";
-      }
     } else {
-      this._inputs = document.querySelectorAll(Search.selectors.MAIN);
+      this._hidden_input = document.querySelector(this._settings.hiddenInputSelector);
+      this._inputs = document.querySelectorAll(this._settings.selector);
       for (let i = this._inputs.length - 1; i >= 0; i--) {
-        this._inputs[i].name = "s";
+        this._inputs[i].value = this._hidden_input.value;
       }
     }
   }
@@ -55,29 +62,26 @@ class Search {
    */
 
   _resetCookie() {
-    Cookies.set('searchresultVisits', 0);
+    Cookies.set(this._settings.cookieName, 0);
   }
 
-  _addNotification(remove) {
-    const info = document.createElement("p");
-    info.classList.add("mb-3");
-    info.innerHTML = "Please verify you are Human."
-    const recaptchaEl = document.getElementById('search-recaptcha');
+  _addNotification() {
+    const info = document.createElement('p');
+    info.classList.add('mb-3');
+    info.innerHTML = this._settings.message;
+    const recaptchaEl = document.getElementById(this._settings.recaptchaContainerId);
     recaptchaEl.parentNode.appendChild(info);
   }
 
   _initRecaptcha() {
+    const script = document.createElement('script');
+    script.src = 'https://www.google.com/recaptcha/api.js' + '?onload=searchCallback&render=explicit';
+    script.async = true;
+    script.defer = true;
 
-    const $script = $(document.createElement('script'));
-    $script.attr('src',
-        'https://www.google.com/recaptcha/api.js' +
-        '?onload=searchCallback&render=explicit').prop({
-      async: true,
-      defer: true
-    });
     window.searchCallback = () => {
-      window.grecaptcha.render(document.getElementById('search-recaptcha'), {
-        'sitekey' : '6LeJddoaAAAAABk7Ot0eXAKiqLQDk0v6IW8cpG9o',
+      window.grecaptcha.render(document.getElementById(this._settings.recaptchaContainerId), {
+        'sitekey' : this._settings.siteKey,
         //Below is the local host key
         // 'sitekey' : '6LcAACYUAAAAAPmtvQvBwK89imM3QfotJFHfSm8C',
         'callback': 'searchRecaptcha',
@@ -97,7 +101,8 @@ class Search {
     };
 
     this._recaptchaRequired = true;
-    $('head').append($script);
+    const head = document.head || document.getElementsByTagName('head').item(0);
+    head.appendChild(script);
     return this;
   }
 
@@ -116,10 +121,14 @@ class Search {
   // }
 }
 
-Search.selectors = {
-  MAIN: '[data-js*="search"]',
-  FORM: '.search-form',
-  HIDDEN_INPUT: '[data-js*="hidden-search-input"]',
-};
+Search.MAIN_INPUT = '[data-js*="search"]';
+Search.FORM = '.search-form';
+Search.HIDDEN_INPUT = '[data-js*="hidden-search-input"]';
+Search.EXPIRATION_DAYS = 1;
+Search.COOKIE_NAME = 'searchresultVisits';
+Search.RECAPTCHA_MESSAGE = 'Please verify you are Human.';
+Search.RECAPTCHA_CONTAINER_ID = 'search-recaptcha';
+Search.SITE_KEY = '6LeJddoaAAAAABk7Ot0eXAKiqLQDk0v6IW8cpG9o';
+Search.VIEW_COUNT = 5;
 
 export default Search;
